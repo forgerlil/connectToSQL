abf3b8cce45434fd885988cf7d2fa4aa10ea4fd1
forgerlil <lilianforger@gmail.com>
2023-03-30 17:21:38 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
